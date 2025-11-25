0560ef526af3bc1af73388709d582bf166a8c277
Yoel Amadeo <yamadeo9@gmail.com>
2025-11-25 18:07:02 +0700

    a

--- src/mlflow_logger.py
+++ src/mlflow_logger.py
@@ -1,23 +1,12 @@
 import mlflow
-import json
-from datetime import datetime
+import os
 
-def log_trends_to_mlflow(file_path="data/trends.json"):
-    with open(file_path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-        if isinstance(data, list):
-            latest = data[-1]
-        else:
-            latest = data
+mlflow_uri = os.getenv("MLFLOW_TRACKING_URI", "https://mlflow-production.up.railway.app")
 
-    with mlflow.start_run(run_name="trends_scrape_run"):
-        mlflow.log_param("country", latest["meta"]["source"])
-        mlflow.log_metric("trend_count", latest["meta"]["count"])
-        mlflow.log_param("timestamp", latest["meta"]["scraped_at"])
-        mlflow.log_artifact(file_path)
-        print("✅ Logged to MLflow")
+mlflow.set_tracking_uri(mlflow_uri)
+mlflow.set_experiment("Twitter_Trends_Scraping")
 
-if __name__ == "__main__":
-    mlflow.set_tracking_uri("http://localhost:5000")  # nanti bisa pakai MLflow server
-    mlflow.set_experiment("Twitter_Trends_Scraping")
-    log_trends_to_mlflow()
+with mlflow.start_run():
+    mlflow.log_param("country", "Indonesia")
+    mlflow.log_metric("trend_count", len(open("data/trends.json").read()))
+    mlflow.log_artifact("data/trends.json")
